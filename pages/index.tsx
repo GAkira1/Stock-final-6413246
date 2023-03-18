d9aed691c77fda7da052f1f53bf9638481a75e6d
GAkira1 <pfuusi_1990@hotmail.com>
2023-03-19 02:42:08 +0700

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,57 @@
 import React from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
+import styles from '../styles/HomePage.module.css';
 
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Suppliers Management System</h1>
-      <p>
-      </p>
-      <ul>
-        <li>
-          <Link href="/supplier-management">
-          </Link>
-        </li>
-        {/* Add more links to different pages as needed */}
-      </ul>
-    </div>
-  );
+interface Supplier {
+  _id: string;
+  supplierName: string;
+  address: string;
+  phoneNumber: string;
 }
+
+interface HomePageProps {
+  suppliers: Supplier[];
+}
+
+const HomePage: React.FC<HomePageProps> = ({ suppliers }) => {
+  return (
+    <>
+      <Head>
+        <title>Suppliers Management</title>
+      </Head>
+      <div className={styles.container}>
+        <h1>Suppliers Management System</h1>
+        <Link href="/supplier">
+          <a>Add New Supplier</a>
+        </Link>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>Supplier Name</th>
+              <th>Address</th>
+              <th>Phone Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            {suppliers.map((supplier) => (
+              <tr key={supplier._id}>
+                <td>
+                  <Link href={`/supplier/${supplier._id}`}>
+                    <a className={styles.link}>{supplier.supplierName}</a>
+                  </Link>
+                </td>
+                <td>{supplier.address}</td>
+                <td>{supplier.phoneNumber}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {/* Add more links to different pages as needed */}
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
+
